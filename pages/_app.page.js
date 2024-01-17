a3597146be3a2e929035c8548fd8234d234cd3a9
Daniel Ching <dancwq2@gmail.com>
2024-01-18 00:11:54 +0800

    Edited title of webpage

--- pages/_app.page.js
+++ pages/_app.page.js
@@ -4,6 +4,9 @@ import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Running Made Simple</title>
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
