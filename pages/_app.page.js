9bcb533843f4f9b198b228317d7660ac8d294460
Daniel Ching <dancwq2@gmail.com>
2024-01-18 14:48:23 +0800

    added page title

--- pages/_app.page.js
+++ pages/_app.page.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
