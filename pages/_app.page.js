ce77745e04b941b159fec786ac8159e0e1a511c1
Daniel Ching <dancwq2@gmail.com>
2024-03-19 15:32:08 +0800

    Added speed insights

--- pages/_app.page.js
+++ pages/_app.page.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import Head from "next/head";
 import { DefaultSeo } from "next-seo";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }) {
       </Head>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
